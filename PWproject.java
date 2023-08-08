ea14020593ff6bc4fdc4e16dba5fb67bd259beec
RahulPWSkill <rj99508178@gmail.com>
2023-08-08 14:24:17 +0530

    first commit

---
+++ PWproject.java
@@ -0,0 +1,12 @@
+public class java
+{  
+  private int marks;
+  private String name;
+  private float percentage;
+  
+  
+  public static void main(String[] args)
+  {
+    java j = new java();
+  }
+}
